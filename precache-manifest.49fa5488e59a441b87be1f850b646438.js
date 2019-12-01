515cbbe709be88073884120e978f11352a38b652
nick-hiebl <n.hiebl@student.unsw.edu.au>
2019-12-02 00:41:07 +1100

    Updates

---
+++ precache-manifest.49fa5488e59a441b87be1f850b646438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c905992fe7ffaea213fedde5c6c4d53f",
+    "url": "/human-sorting/index.html"
+  },
+  {
+    "revision": "47c40f9b1414fead803f",
+    "url": "/human-sorting/static/css/main.b01693bb.chunk.css"
+  },
+  {
+    "revision": "6744e82c5e5f518a7fa5",
+    "url": "/human-sorting/static/js/2.cde95c5c.chunk.js"
+  },
+  {
+    "revision": "47c40f9b1414fead803f",
+    "url": "/human-sorting/static/js/main.1cff0971.chunk.js"
+  },
+  {
+    "revision": "d56730d383aec75b067e",
+    "url": "/human-sorting/static/js/runtime-main.76bed65e.js"
+  }
+]);
